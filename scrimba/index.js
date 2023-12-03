5c826e6bef2961e6f292815a7907ab3c7804ab88
Brian Cheruiyot <101213526+Bryan-cheru@users.noreply.github.com>
2023-12-03 11:59:48 +0300

    Add files via upload

---
+++ scrimba/index.js
@@ -0,0 +1,63 @@
+//document.getElementById("count-el").innerText = 5
+/**
+let count = 656
+count = count + 76
+console.log(count)
+
+let myAge = 21
+console.log(myAge) 
+let bonuspoints = 50
+bonuspoints = bonuspoints + 20
+console.log(bonuspoints) 
+**/
+
+function increment() {
+    console.log("the button was clickr")
+
+
+}
+/**
+
+
+
+function countdown() {
+    console.log(5)
+    console.log(4)
+    console.log(3)
+    console.log(2)
+    console.log(1)
+}
+
+countdown()
+
+
+// GO! 🏁
+// Players are running the race 🏎 💨
+// Race is finished! 🍾
+
+// Get ready for a new race 🏎 🏎 🏎
+
+countdown()
+
+
+function callibrace() {
+    console.log(42)
+
+
+}
+
+callibrace()
+
+**/
+let lapscompleted = 0
+
+
+// Create a function that logs out the sum of all the lap times
+function incrementlaps() {
+    // let totalTime = lap1 + lap2 + lap3
+    console.log(lapscompleted = lapscompleted + 1)
+}
+
+incrementlaps()
+incrementlaps()
+incrementlaps()
